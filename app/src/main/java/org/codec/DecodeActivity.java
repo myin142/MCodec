3952e8d3cca630a7889f1bfed19e3f650cf7cc05
Min <illuslion1998@gmail.com>
2017-08-22 14:09:08 +0200

    add examples + comments

--- app/src/main/java/org/codec/DecodeActivity.java
+++ app/src/main/java/org/codec/DecodeActivity.java
@@ -4,11 +4,11 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import org.codec.media.FrameGrab;
 
 public class DecodeActivity extends AppCompatActivity{
+    // Video Files
     private static String videoFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/videos/";
     private static String hdLowVideo = "cats_with_timecode-1920x1080-30fps-baseline-4mbps.mp4";
     private static String hdHighVideo = "cats_with_timecode-1920x1080-30fps-main-14mbps.mp4";
@@ -16,7 +16,7 @@ public class DecodeActivity extends AppCompatActivity{
     private static String lowVideo = "cats_with_timecode-640x320-30fps-baseline-4mbps.mp4";
     private static String hdHigh24 = "cats_with_timecode-1920x1080-24fps-baseline-14mpbs.mp4";
 
-    private static final String SAMPLE = videoFolder + lowVideo;
+    private static String SAMPLE = videoFolder + lowVideo;
     private static String TAG = "DecodeActivity";
 
     FrameGrab grab = null;
@@ -28,19 +28,27 @@ public class DecodeActivity extends AppCompatActivity{
 
         int frame = 0;
         int frameEnd = 10;
+        int frameMiddle = frameEnd/2;
+
         grab = new FrameGrab();
+
+        // Create Decoder for File
+        // Re-Run everytime new file is used
         grab.setSource(SAMPLE);
         grab.init();
-        grab.seekToFrame(frame);
 
-        long startTime = System.currentTimeMillis();
+        // Get Range of Frames
+        grab.seekToFrame(frame);
         for(int i = frame; i <= frameEnd; i++){
             grab.getFrameAt(i);
-            //grab.saveBitmap(videoFolder + "test.jpg");
+            grab.saveBitmap(videoFolder + "test/frame"+i+".jpg");
         }
-        long endTime = System.currentTimeMillis();
-        long totalTime = (endTime - startTime) / 1000;
-        Log.d(TAG, "Total Time: " + totalTime + "s");
+
+        // Get one frame
+        grab.seekToFrame(frameMiddle);
+        grab.getFrameAt(frameMiddle);
+        Bitmap image = grab.getBitmap();
+
         grab.release();
 
     }
